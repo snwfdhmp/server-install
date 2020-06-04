33f6e79b90427db07550b01537d581ff76d7a121
Martin Joly <snwfdhmp@gmail.com>
2020-06-04 21:48:17 +0200

    add 'get'

--- run.sh
+++ run.sh
@@ -10,11 +10,11 @@ while read line; do
     cmd=$(echo $line | cut -d' ' -f1)
     case $cmd in
         ENV)
-            export $(echo $line | cut -d' ' -f2) $(echo $line | cut -d' ' -f3-)
+            echo export $(echo $line | cut -d' ' -f2) $(echo $line | cut -d' ' -f3-)
         ;;
         COOK)
             cookbook=$(echo $line | cut -d' ' -f2)
-            "find ./cookbooks/$cookbook -name '*.sh' | sort | xargs -I{} -- /bin/bash {}"
+            echo "find ./cookbooks/$cookbook -name '*.sh' | sort | xargs -I{} -- /bin/bash {}"
         ;;
         *)
         echo "unknown command $cmd"
