76569b32d8131bcc033b7424aeb31a62d5868fab
Martin Joly <snwfdhmp@gmail.com>
2020-06-04 23:44:27 +0200

    update cookbooks and recipes

---
+++ cookbooks/user-admin/0-configure.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+USER_ADMIN_NAME=${USER_ADMIN_NAME:-"snwfdhmp"}
+USER_ADMIN_SHELL=${USER_ADMIN_NAME:-"/bin/bash"}
+USER_ADMIN_COPY_ROOT_SSH_AUTHORIZED_KEYS=${USER_ADMIN_COPY_ROOT_SSH_AUTHORIZED_KEYS:-1}
+useradd -m -s $USER_ADMIN_SHELL -G sudo $USER_ADMIN_NAME
+
+if [ "$USER_ADMIN_COPY_ROOT_SSH_AUTHORIZED_KEYS" -eq 1 ] && [ -f /root/.ssh/authorized_keys ]; then
+    mkdir -p /home/$USER_ADMIN_NAME/.ssh
+    cp /root/.ssh/authorized_keys /home/$USER_ADMIN_NAME/.ssh
+fi
